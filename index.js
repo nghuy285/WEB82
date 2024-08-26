d34edb4911c55be9092b6d2af03df3ae3ca89748
Huy Nguyễn <huyngo28052005@gmail.com>
2024-08-27 04:12:15 +0700

    update

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ import express from 'express';
 import mongoose from 'mongoose';
 import userRoutes from './routes/userRoutes.js';
 import postRoutes from './routes/postRoutes.js';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
+const port = process.env.PORT 
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://huyngo285:Giahuy123@web82.de2cj.mongodb.net/?retryWrites=true&w=majority&appName=web82');
+mongoose.connect(process.env.MONGO_URI);
 
 app.use('/users', userRoutes);
 app.use('/posts', postRoutes);
 
-app.listen(8080, () => {
+app.listen(port, () => {
   console.log('Server is running on port 8080');
 });
